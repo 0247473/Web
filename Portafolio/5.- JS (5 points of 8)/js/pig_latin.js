a0b205ee82ca69f4666544fc0c60333b3f131d8b
0247473 <0247473@up.edu.mx>
2023-10-20 12:48:03 -0600

    Examen Parcial 2 y parte del portafolio

---
+++ Portafolio/5.- JS (5 points of 8)/js/pig_latin.js
@@ -0,0 +1,34 @@
+/*
+Pig Latin
+*/
+
+function igpayAtinlay(txtVal) {
+  // TODO: Initialize the word array properly
+  var returnArray = [],
+    wordArray = txtVal.split(' ');
+  // TODO: make sure that the output is being properly built to produce the desired result.
+  for (var i = 0; i < wordArray.length; i++) {
+    var word = wordArray[i];
+    var beginning = word.charAt(0);
+
+    if (/[aeiouAEIOU]/.test(beginning)) {
+      returnArray.push(word += 'way');
+      continue;
+    }
+
+    for (var ii = 1; ii < word.length; ii++) {
+      if (/[aeiouAEIOU]/.test(word.charAt(ii))) {
+        break;
+      } else {
+        beginning += word.charAt(ii);
+      }
+    }
+    returnArray.push(word.substr(beginning.length) + beginning + 'ay');
+  }
+  document.getElementById('pigLatLbl').innerHTML = returnArray.join("  ");
+}
+
+// Some examples of expected outputs
+console.log(igpayAtinlay("pizza")); // "izzapay"
+console.log(igpayAtinlay("apple")); // "appleway"
+console.log(igpayAtinlay("happy meal")); // "appyhay ealmay"
